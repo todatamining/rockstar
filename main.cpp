43869548f9da8ff3ef0b598aff6849217b0ead02
tdm <you@example.com>
2015-07-18 08:38:53 -0400

    varidic template

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+
+using namespace std;
+
+template<typename ... T>
+void dummy(T ... t)
+{
+
+}
+
+template<typename T>
+void some_function(T t)
+{
+	cout<< t << "\t";
+}
+
+
+template<typename... Args> 
+void expand(Args&&... args) 
+{
+	dummy( (some_function(args), 1)... );
+	cout<<endl;
+}
+
+
 int main()
 {
-  std::cout << "Hello World!";
+	expand(42, "answer", true);
 }
