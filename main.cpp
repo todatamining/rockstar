dbb3e9a74df5c97fd31c5d5b9bbbe699ed2ddbc0
tdm <you@example.com>
2015-07-18 15:54:03 -0400

    add __func__     __cplusplus

--- main.cpp
+++ main.cpp
@@ -2,15 +2,29 @@
 
 using namespace std;
 
+#define PRINT_FUNC  \
+	cout<< __func__ <<endl
+
+class CAT
+{
+	public:
+		void print()
+		{
+			PRINT_FUNC;	
+		}
+};
+
+
 template<typename ... T>
 void dummy(T ... t)
 {
-
+	PRINT_FUNC;
 }
 
 template<typename T>
 void some_function(T t)
 {
+	PRINT_FUNC;
 	cout<< t << "\t";
 }
 
@@ -18,6 +32,7 @@ void some_function(T t)
 template<typename... Args> 
 void expand(Args&&... args) 
 {
+	PRINT_FUNC;
 	dummy( (some_function(args), 1)... );
 	cout<<endl;
 }
@@ -25,5 +40,11 @@ void expand(Args&&... args)
 
 int main()
 {
+#if __cplusplus < 201103L
+#error "should use c++ implementaton"
+#endif
+	PRINT_FUNC;
 	expand(42, "answer", true);
+	CAT c;
+	c.print();
 }
